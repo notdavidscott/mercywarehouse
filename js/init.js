37dcf1e7244b80bb310cc2fd8869a0c8fab17a6b
David Scott Perez <david.scott.perez@live.com>
2018-12-12 16:00:18 -0800

    added some fade elements

--- js/init.js
+++ js/init.js
@@ -7,6 +7,75 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
+
+// animation!! 
+
+$(document).ready(function() {
+  
+
+  $("#toMissionContainer").animate({
+   top: '20px',  
+  }, 300 );
+
+
+   /* Every time the window is scrolled ... */
+   $(window).scroll( function(){
+    
+    /* Check the location of each desired element */
+    $('.hideme').each( function(i){
+        
+        var bottom_of_object = $(this).position().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        
+        /* If the object is completely visible in the window, fade it it */
+        if( bottom_of_window > bottom_of_object ){
+            
+            $(this).animate({'opacity':'1'},1000);
+                
+        }
+        
+    }); 
+
+    $('.hidemedelay1').each( function(i){
+        
+      var bottom_of_object = $(this).position().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      
+      /* If the object is completely visible in the window, fade it it */
+      if( bottom_of_window > bottom_of_object ){
+          
+          $(this).animate({'opacity':'1'},1600);
+              
+      }
+      
+  }); 
+
+  $('.hidemedelay2').each( function(i){
+        
+    var bottom_of_object = $(this).position().top + $(this).outerHeight();
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+    
+    /* If the object is completely visible in the window, fade it it */
+    if( bottom_of_window > bottom_of_object ){
+        
+        $(this).animate({'opacity':'1'},2200);
+            
+    }
+    
+}); 
+
+
+});
+
+
+
+
+
+
+
+});
+
+
 //get current year for copyright 
 
 let currentYear = "Copyright " + (new Date()).getFullYear() + " &#169; Mercy Warehouse";
